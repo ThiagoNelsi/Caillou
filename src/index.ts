eb7c743a3117ab2630cc71c0fe8a30ab0622dff7
ThiagoNelsi <thiagonelsi@gmail.com>
2021-10-17 00:30:24 -0300

    Implements app's core logic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
-const hello = () => 'Hello, World';
+import ping from 'ping';
 
-console.log(hello());
+const TARGETS = ['google.com', 'github.com'];
+const offline: string[] = [];
+
+main();
+
+async function main() {
+
+    while (true) {
+        // Ping all targets
+        TARGETS.forEach(async target => {
+            ping.sys.probe(target, (isAlive) => {
+                if (!isAlive) {
+                    if (offline.includes(target)) return;
+                    handleError(target);
+                } else if (offline.includes(target)) {
+                    offline.splice(offline.indexOf(target), 1);
+                }
+            }, { timeout: 5 });
+        });
+        await sleep(5);
+    }
+
+}
+
+function handleError(target: string) {
+    offline.push(target);
+    sendAlertEmail(target);
+}
+
+function sendAlertEmail(target: string) {
+    console.log(`sending email => ${target} is down!`);
+}
+
+function sleep(seconds: number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, seconds * 1000);
+    });
+}
