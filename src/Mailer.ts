b1966a0f161773c7dc6f6152f05045ecfed988c6
ThiagoNelsi <thiagonelsi@gmail.com>
2021-10-18 22:58:07 -0300

    Change default indent size to 2 spaces

--- src/Mailer.ts
+++ src/Mailer.ts
@@ -1,73 +1,73 @@
 import nodemailer, { Transporter } from 'nodemailer';
 
 interface ICreateMailer {
-    host: string;
-    port: number;
-    auth: IAuth,
+  host: string;
+  port: number;
+  auth: IAuth,
 }
 
 interface IAuth {
-    user: string;
-    password: string;
+  user: string;
+  password: string;
 }
 
 interface ISendMail {
-    to: string;
-    subject: string;
-    content: string;
-    isHtml?: boolean;
+  to: string;
+  subject: string;
+  content: string;
+  isHtml?: boolean;
 }
 
 export class Mailer {
 
-    private mailer: Transporter;
+  private mailer: Transporter;
 
-    private constructor(host: string, port: number, auth: IAuth) {
-        this.mailer = nodemailer.createTransport({
-            host,
-            port,
-            auth: {
-                type: 'login',
-                user: auth.user,
-                pass: auth.password,
-            }
-        });
+  private constructor(host: string, port: number, auth: IAuth) {
+    this.mailer = nodemailer.createTransport({
+      host,
+      port,
+      auth: {
+        type: 'login',
+        user: auth.user,
+        pass: auth.password,
+      }
+    });
+  }
+
+  sendMail({ to, subject, content, isHtml = false }: ISendMail) {
+
+    interface IConfig {
+      to: string;
+      subject: string;
+      html?: string;
+      text?: string;
     }
 
-    sendMail({ to, subject, content, isHtml = false }: ISendMail) {
-
-        interface IConfig {
-            to: string;
-            subject: string;
-            html?: string;
-            text?: string;
-        }
-
-        const config: IConfig = {
-            to,
-            subject,
-        }
-
-        if (isHtml) config.html = content;
-        else config.text = content;
-
-        return this.mailer.sendMail(config);
-        
+    const config: IConfig = {
+      to,
+      subject,
     }
 
-    private verify() {
-        return new Promise((resolve, reject) => {
-            this.mailer.verify((err, success) => {
-                if (success) return resolve(true);
-                return reject(err);
-            });
-        })
-    }
+    if (isHtml) config.html = content;
+    else config.text = content;
 
-    static async createMailer({ host, port, auth }: ICreateMailer) {
-        const mailer = new Mailer(host, port, auth);
-        if (await mailer.verify()) return mailer;
-        else throw new Error('Unable to create mailer');
-    }
+    return this.mailer.sendMail(config);
+
+  }
+
+  private verify() {
+    return new Promise((resolve, reject) => {
+      this.mailer.verify((err, success) => {
+        if (success) return resolve(true);
+        return reject(err);
+      });
+    })
+  }
+
+  static async createMailer({ host, port, auth }: ICreateMailer) {
+    const mailer = new Mailer(host, port, auth);
+    if (await mailer.verify()) return mailer;
+    else throw new Error('Unable to create mailer');
+  }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -5,68 +5,68 @@ import { Mailer } from './Mailer';
 configEnv();
 
 const targets: {
-    domain: string,
-    alertEmail: string,
+  domain: string,
+  alertEmail: string,
 }[] = [];
 
 const offline: string[] = [];
 
 targets.push({
-    domain: 'github.com',
-    alertEmail: 'some_random@email.com',
+  domain: 'github.com',
+  alertEmail: 'some_random@email.com',
 });
 
 main();
 
 async function main() {
 
-    const mailer = await Mailer.createMailer({
-        host: process.env.SMTP_HOST as string,
-        port: Number(process.env.SMTP_PORT),
-        auth: {
-            user: process.env.SMTP_AUTH_USER as string,
-            password: process.env.SMTP_AUTH_PASSWORD as string,
+  const mailer = await Mailer.createMailer({
+    host: process.env.SMTP_HOST as string,
+    port: Number(process.env.SMTP_PORT),
+    auth: {
+      user: process.env.SMTP_AUTH_USER as string,
+      password: process.env.SMTP_AUTH_PASSWORD as string,
+    }
+  });
+
+  while (true) {
+    // Ping all targets
+    targets.forEach(async ({ domain, alertEmail }) => {
+      ping.sys.probe(domain, (isAlive) => {
+        if (!isAlive) {
+          if (offline.includes(domain)) return;
+          handleError(domain, alertEmail);
+        } else if (offline.includes(domain)) {
+          offline.splice(offline.indexOf(domain), 1);
         }
+      }, { timeout: 5 });
     });
+    await sleep(5);
+  }
 
-    while (true) {
-        // Ping all targets
-        targets.forEach(async ({ domain, alertEmail }) => {
-            ping.sys.probe(domain, (isAlive) => {
-                if (!isAlive) {
-                    if (offline.includes(domain)) return;
-                    handleError(domain, alertEmail);
-                } else if (offline.includes(domain)) {
-                    offline.splice(offline.indexOf(domain), 1);
-                }
-            }, { timeout: 5 });
-        });
-        await sleep(5);
-    }
+  function handleError(domain: string, alertEmail: string) {
+    offline.push(domain);
+    sendAlertEmail(domain, alertEmail);
+  }
 
-    function handleError(domain: string, alertEmail: string) {
-        offline.push(domain);
-        sendAlertEmail(domain, alertEmail);
-    }
-    
-    function sendAlertEmail(domain: string, targetEmail: string) {
-        const html = `
+  function sendAlertEmail(domain: string, targetEmail: string) {
+    const html = `
             <h2 color="red">Calliou Alert!</h2>
             <hr>
             <h3>We have just detected (at ${new Date().toLocaleString()}) that ${domain} is offline!</h3>
         `;
-        mailer.sendMail({
-            to: targetEmail,
-            subject: domain + ' offline!',
-            content: html,
-            isHtml: true,
-        }).then(console.log).catch(console.log);
-    }
+    mailer.sendMail({
+      to: targetEmail,
+      subject: domain + ' offline!',
+      content: html,
+      isHtml: true,
+    }).then(console.log).catch(console.log);
+  }
 
 }
 
 function sleep(seconds: number) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, seconds * 1000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, seconds * 1000);
+  });
 }
